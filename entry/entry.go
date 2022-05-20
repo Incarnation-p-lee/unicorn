199851fea903c3e1b94a79f23535dd5a93b5a32c
Pan Li <Incarnation-p-lee@users.noreply.github.com>
2022-05-20 16:35:45 +0800

    Add entry point from boot to main. (#11)
    * Add entry point from boot to main.
    * Adjust makefile
    * Adjust boot asm
    * Make boot asm invoke main.c
    * Make main.c invoke entry.go
    * Add sudo apt-get install gcc-multilib to action.
    * Add -lpthread to make github action happy.
    
    Signed-off-by: Pan Li <incarnation.p.lee@outlook.com>

---
+++ entry/entry.go
@@ -0,0 +1,11 @@
+package main
+
+import "C"
+
+//export Entry
+func Entry() {
+}
+
+func main() {
+}
+
---
+++ entry/main.c
@@ -0,0 +1,9 @@
+// The Entry defined in golang, and build with cgo.
+extern void Entry();
+
+int main() {
+    Entry();
+
+    return 0;
+}
+
