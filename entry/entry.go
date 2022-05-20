3b165529d3d7b145f78c36a26b7398c0c30bf8d4
Pan Li <incarnation.p.lee@outlook.com>
2022-05-20 08:17:05 +0000

    Resolve reported issue from Deepgo.
    Signed-off-by: Pan Li <incarnation.p.lee@outlook.com>

--- entry/entry.go
+++ entry/entry.go
@@ -1,14 +1,11 @@
 package main
 
-// #cgo CFLAGS: -m32 -fno-stack-protector
 import "C"
 
 //export Entry
-func Entry(magic int) {
+func Entry() {
 }
 
-
-// XXXXX#cgo CFLAGS: -nostdlib -nostdinc -fno-builtin -fno-stack-protector -m32 -Wall -Wextra -Werror
 func main() {
 }
 
