0a70e3533f2870a5293268ad1b6627d760e4ca8e
Pan Li <incarnation.p.lee@outlook.com>
2022-05-23 09:34:05 +0000

    Resolve comments.
    Signed-off-by: Pan Li <incarnation.p.lee@outlook.com>

--- test/test.c
+++ test/test.c
@@ -7,10 +7,11 @@ expand_test_suite(s_test_suite_t *suite)
 {
     uint32 size = suite->size;
     uint32 new_size = size * 2;
-    uint32 new_size_in_bytes = sizeof(s_test_result_t) * new_size;
+    uint32 result_in_bytes = sizeof(s_test_suite_t);
+    uint32 new_size_in_bytes = result_in_bytes * new_size;
     s_test_result_t *new_address = (s_test_result_t *)malloc(new_size_in_bytes);
 
-    memcpy(new_address, suite->all_results, sizeof(s_test_result_t) * size);
+    memcpy(new_address, suite->all_results, result_in_bytes * size);
 
     free(suite->all_results);
 
